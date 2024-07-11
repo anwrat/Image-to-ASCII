90f5840372b6bd1c7bbaa864bc60f7cb926f743f
anwrat <anweshrwt22@gmail.com>
2024-07-11 22:00:09 +0545

    Brightness of Image

--- main.py
+++ main.py
@@ -1,4 +1,22 @@
 import cv2
 import numpy as np
 
-print("Trying the git push thing")
+image_path="eiffel.png"
+#Reading image file from path
+image=cv2.imread(image_path)
+#resizing the image, we use interpolation INTER_AREA to shrink the image
+image=cv2.resize(image,(100,100),interpolation=cv2.INTER_AREA)
+print(image)#this gives a 3 dimensional value which represents rgb values of a pixel
+print(image.shape)#the structure, dimensions of the image
+#Creating an empty array 
+brightness=np.empty((image.shape[0],image.shape[1]))
+ascii_image=[]
+#Calculating brightness values by taking average of rgb of a pixel
+for x in range(len(image)):
+    for y in range(len(image[x])):
+        pixel=image[x][y]
+        avg=(int(pixel[0])+int(pixel[1])+int(pixel[2]))//3#(R+G+B)/3
+        brightness[x][y]=avg
+print("The brightness values of all pixels in the image are: ")
+print(brightness)
+print(brightness.shape)
